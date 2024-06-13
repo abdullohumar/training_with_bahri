072a701d9b698fa897a6717aa9d420eac0f100ce
Abdulloh Umar <iniautok@gmail.com>
2024-06-13 10:20:22 +0700

    '201'

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -63,7 +63,10 @@ class AuthController extends Controller
 
         $token = $user->createToken('auth_token')->plainTextToken;
 
-        return response()->json(['status' => 'success', 'user' => $user, 'token' => $token], 200);
+        return response()->json([
+            'user' => $user, 
+            'access_token' => $token
+        ],201);
     }
 
     public function logout(Request $request)
